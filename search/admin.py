3d7dbed7b9e98c68e442b594ac9afb7a3a0f3153
itumizu <s1611488@klis.tsukuba.ac.jp>
2019-05-15 22:05:27 +0900

    和暦モデルと作者モデルを追加

--- search/admin.py
+++ search/admin.py
@@ -1,15 +1,33 @@
 from django.contrib import admin
-from .models import Collection, Haikai, Tanka
+from .models import Collection, Author, Year, Haikai, Tanka
 
-@admin.register(Haikai)
-class HaikaiAdmin(admin.ModelAdmin):
-    list_display = ('firstPart', 'secondPart', 'lastPart', 'firstPartKana', 'secondPartKana', 'lastPartKana', 'collection', )
-    search_fields = ('firstPart', 'secondPart', 'lastPart', 'firstPartKana', 'secondPartKana', 'lastPartKana', 'collection__name', 'collection__parent__name', )
+class CollectionInline (admin.TabularInline):
+    model = Collection
+    extra = 0
+
+class HaikaiInline (admin.TabularInline):
+    model = Haikai
+    extra = 0
 
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ('name', 'parent',)
     search_fields = ('name', 'parent__name', )
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ('name', )
+
+@admin.register(Year)
+class YearAdmin(admin.ModelAdmin):
+    list_display = ('jpnYear', 'adYear', )
+    search_fields = ('jpnYear', 'adYear', )
+
+@admin.register(Haikai)
+class HaikaiAdmin(admin.ModelAdmin):
+    list_display = ('firstPart', 'secondPart', 'lastPart', 'firstPartKana', 'secondPartKana', 'lastPartKana', 'author', 'collection', 'year')
+    search_fields = ('firstPart', 'secondPart', 'lastPart', 'firstPartKana', 'secondPartKana', 'lastPartKana', 'author__name', 'collection__name', 'collection__parent__name', )
 
 @admin.register(Tanka)
 class TankaAdmin(admin.ModelAdmin):
