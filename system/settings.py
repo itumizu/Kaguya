aa6c1434373b8cc119ebc911307b000b1ca1073a
itumizu <s1611488@klis.tsukuba.ac.jp>
2019-06-17 00:24:29 +0900

    HTTPS関連設定の追加

--- system/settings.py
+++ system/settings.py
@@ -37,12 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'request',
     'sass_processor',
     'debug_toolbar',
     'markdownx',
     'axes',
-    'search'
+    'search',
+    'users',
 ]
 
 AUTHENTICATION_BACKENDS = [
@@ -58,7 +58,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'request.middleware.RequestMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'axes.middleware.AxesMiddleware',
 ]
@@ -156,7 +155,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-# AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'users.User'
 
 PAGE_PER_ITEM = 50
 
@@ -201,3 +200,21 @@ AXES_META_PRECEDENCE_ORDER = (
 #     }
 # }
 # AXES_CACHE = 'axes_cache'
+
+ALLOWED_IP_BLOCKS = []
+
+if not DEBUG:
+    SECURE_HSTS_SECONDS = 604800
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+
+    SECURE_BROWSER_XSS_FILTER = True
+
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+
+    X_FRAME_OPTIONS = 'DENY'
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
