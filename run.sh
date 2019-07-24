3bfcdf8faabb6f7d187982ab8bd203836d6679ea
itumizu <s1611488@klis.tsukuba.ac.jp>
2019-07-24 14:06:05 +0900

    Reactコンテナの追加

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 echo "Waiting for PostgreSQL"
 sleep 10
 echo "PostgreSQL is up - executing command"
-exec bash -c "python manage.py migrate && gunicorn system.wsgi -b 0.0.0.0:3031"
+exec bash -c "python manage.py migrate && gunicorn --reload system.wsgi -b 0.0.0.0:3031"
